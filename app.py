e02b9bbe98c24e61466dae3a41c86054fd5e3096
osmarmcn <osmarn84@gmail.com>
2023-12-28 17:56:12 -0300

    projeto inicial

--- app.py
+++ app.py
@@ -13,3 +13,24 @@ for linha in pagina_clientes.iter_rows(min_row=2):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+''''
+iter_rows() - é uma função do openpyxl que vai passar por todas as linhas da lista.
+obs: o parâmetro min_row=2 indica que vai começar na linha dois, tem que vericar na lista onde realmente começa a lista pdoendo ser uma linha diferente.
+
+
+
+
+'''
